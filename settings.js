7c3bd81cdee00ca2ddcb8233f2578a807020adaf
FOREX78 <jjjjdudvkfz@gmail.com>
2025-02-27 09:04:46 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -43,7 +43,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'Manu-MD&JGdVVKzD#PsXmCXM_2Q92qTXwwdtdJZ_mdDNbS_vHkXP3WrWwr6w' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SUDO: process.env.SUDO === undefined ? '94742274855' : process.env.SUDO,
